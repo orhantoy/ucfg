0019bb063da15f0d6094390d72cd3e765ca7af70
Orhan Toy <toyorhan@gmail.com>
2020-07-23 01:38:48 +0200

    Add base validation (pending) specs

--- lib/ucfg.rb
+++ lib/ucfg.rb
@@ -3,4 +3,7 @@ require "ucfg/version"
 module Ucfg
   class Error < StandardError; end
   # Your code goes here...
+
+  # def self.validate(config, schema)
+  # end
 end
---
+++ spec/base_validation_spec.rb
@@ -0,0 +1,122 @@
+require "json"
+
+RSpec.describe "Base validation" do
+  xit "fails if required property is missing" do
+    config = <<-JSON
+    {
+      "version": "7.9"
+    }
+    JSON
+
+    schema = <<-JSON
+    {
+      "required": ["name"],
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+    JSON
+
+    result = Ucfg.validate(JSON.parse(config), JSON.parse(schema))
+
+    expect(result.valid?).to eq(false)
+    expect(result.errors).to eq(["Required property `name` is missing"])
+  end
+
+  xit "fails if additional properties are disabled in schema but still provided" do
+    config = <<-JSON
+    {
+      "version": "7.9"
+    }
+    JSON
+
+    schema = <<-JSON
+    {
+      "additionalProperties": false,
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+    JSON
+
+    result = Ucfg.validate(JSON.parse(config), JSON.parse(schema))
+
+    expect(result.valid?).to eq(false)
+    expect(result.errors).to eq(["Property `version` is not supported"])
+  end
+
+  xit "fails if string property is provided as other type" do
+    config = <<-JSON
+    {
+      "name": true
+    }
+    JSON
+
+    schema = <<-JSON
+    {
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+    JSON
+
+    result = Ucfg.validate(JSON.parse(config), JSON.parse(schema))
+
+    expect(result.valid?).to eq(false)
+    expect(result.errors).to eq(["Property `name` must be of type `string` (provided value `true` of type `boolean`)"])
+  end
+
+  xit "fails if with multiple errors" do
+    config = <<-JSON
+    {
+      "name": true
+    }
+    JSON
+
+    schema = <<-JSON
+    {
+      "required": ["version"],
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+    JSON
+
+    result = Ucfg.validate(JSON.parse(config), JSON.parse(schema))
+
+    expect(result.valid?).to eq(false)
+    expect(result.errors).to eq(["Required property `version` is missing", "Property `name` must be of type `string` (provided value `true` of type `boolean`)"])
+  end
+
+  xit "passes if config follows schema" do
+    config = <<-JSON
+    {
+      "name": "ucfg"
+    }
+    JSON
+
+    schema = <<-JSON
+    {
+      "required": ["name"],
+      "properties": {
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+    JSON
+
+    result = Ucfg.validate(JSON.parse(config), JSON.parse(schema))
+
+    expect(result.valid?).to eq(true)
+    expect(result.errors).to eq([])
+  end
+end
