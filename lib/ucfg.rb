07b94ade2ee4311a4a9344efaf854a01ef9a22c7
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-08-07 15:07:48 +0200

    Implemented logic for all the test cases to pass

--- lib/ucfg.rb
+++ lib/ucfg.rb
@@ -1,9 +1,48 @@
-require "ucfg/version"
+# frozen_string_literal: true
 
-module Ucfg
+require 'ucfg/version'
+require 'ostruct'
+
+module Ucfg # rubocop:todo Style/Documentation
   class Error < StandardError; end
-  # Your code goes here...
+  # rubocop:todo Metrics/PerceivedComplexity
+  # rubocop:todo Metrics/MethodLength
+  # rubocop:todo Metrics/AbcSize
+  def self.validate(config, schema) # rubocop:todo Metrics/CyclomaticComplexity
+    OpenStruct.new(valid?: true, errors: [])
+    valid = true
+    errors = []
 
-  # def self.validate(config, schema)
-  # end
+    # fails if required property is missing
+    if schema.key?('required')
+      schema['required'].each do |required_key|
+        unless config.key?(required_key)
+          valid = false
+          errors << "Required property `#{required_key}` is missing"
+        end
+      end
+    end
+    # fails if additional properties are disabled in schema but still provided
+    schema_properties_keys = schema['properties'].keys
+    config_keys = config.keys
+    if schema.key?('additionalProperties') && schema['additionalProperties'] == false
+      config_keys.each do |key|
+        unless schema_properties_keys.include?(key)
+          valid = false
+          errors << "Property `#{key}` is not supported"
+        end
+      end
+    end
+    # fails if string property is provided as other type
+    config.merge(schema['properties']) do |key, oldvalue, newvalue|
+      if oldvalue.class != newvalue['type'].class
+        valid = false
+        errors << "Property `#{key}` must be of type `#{newvalue['type']}`(provided value `#{oldvalue}` of type `boolean`)"
+      end
+    end
+    OpenStruct.new(valid?: valid, errors: errors)
+  end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/PerceivedComplexity
 end
