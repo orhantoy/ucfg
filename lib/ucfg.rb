52c2ccbdaac1d4de2b8c80bf09ca30641f6aa33a
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-08-12 14:56:04 +0200

    Included logic for type checking in test case 5

--- lib/ucfg.rb
+++ lib/ucfg.rb
@@ -8,7 +8,8 @@ module Ucfg # rubocop:todo Style/Documentation
   # rubocop:todo Metrics/PerceivedComplexity
   # rubocop:todo Metrics/MethodLength
   # rubocop:todo Metrics/AbcSize
-  def self.validate(config, schema) # rubocop:todo Metrics/CyclomaticComplexity
+  def self.validate(config, schema)
+    # rubocop:todo Metrics/CyclomaticComplexity
     OpenStruct.new(valid?: true, errors: [])
     valid = true
     errors = []
@@ -35,10 +36,12 @@ module Ucfg # rubocop:todo Style/Documentation
       end
     end
     # fails if string property is provided as other type
-    config.merge(schema["properties"]) do |key, oldvalue, newvalue|
-      if oldvalue.class != newvalue["type"].class
-        valid = false
-        errors << "Property `#{key}` must be of type `#{newvalue['type']}` (provided value `#{oldvalue}` of type `boolean`)"
+    config.each do |key, value|
+      if schema["properties"].key?(key) && schema["properties"][key].key?("type")
+        if schema["properties"][key]["type"] != value_type(value)
+          valid = false
+          errors << "Property `#{key}` must be of type `#{schema['properties'][key]['type']}` (provided value `#{value}` of type `#{value_type(value)}`)"
+        end
       end
     end
     OpenStruct.new(valid?: valid, errors: errors)
@@ -46,4 +49,9 @@ module Ucfg # rubocop:todo Style/Documentation
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
   # rubocop:enable Metrics/PerceivedComplexity
+
+  def self.value_type(value)
+    return "string" if value.is_a?(String)
+    return "boolean" if value.is_a?(TrueClass) || value.is_a?(FalseClass)
+  end
 end
